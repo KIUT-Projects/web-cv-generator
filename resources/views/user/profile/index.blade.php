8789c5fe22ef10af86a6edd2bf758f00b22b65ef
uzsoftic <softuzb@gmail.com>
2023-04-19 23:35:41 +0500

    last

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -14,45 +14,87 @@
     @endphp
 
     <div class="container-fluid">
+
         <div class="row">
-
-            @foreach($profiles as $profile)
-                <div class="col-12 col-md-6 col-lg-4 my-3">
-                    <div class="card">
-                        <div class="card-header pb-0 px-3">
-                            <h6 class="mb-0">{{ __('Umarov Kamoliddin') }}</h6>
-                        </div>
-                        <div class="card-body pt-4 p-3">
-                            <ul style="list-style-type: none; padding: 0;">
-                                <li><i class="fal fa-user-edit"></i> Name: Kamoliddin</li>
-                                <li><i class="fal fa-user-edit"></i> Surname: Umarov</li>
-                                <li><i class="fal fa-calendar"></i> Birthday: {{ now()->format('d.m.Y') }}</li>
-                                <li><i class="fal fa-pen"></i> Updated at: {{ now()->format('d.m.Y') }}</li>
-                            </ul>
-
-                            <div class="row">
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button" class="btn btn-secondary"><i class="fas fa-trash"></i></button>
-                                    <button type="button" class="btn btn-secondary"><i class="fas fa-pen"></i></button>
-                                    <button type="button" class="btn btn-primary"><i class="fas fa-clipboard-check"></i> Make</button>
-                                </div>
+            <div class="col-12">
+                <div class="card mb-4 mx-4">
+                    <div class="card-header pb-0">
+                        <div class="d-flex flex-row justify-content-between">
+                            <div>
+                                <h5 class="mb-0">All Users</h5>
                             </div>
+                            <a href="#" class="btn bg-gradient-primary btn-sm mb-0" type="button">+&nbsp; New User</a>
+                        </div>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        ID
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Photo
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Name
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Email
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        role
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Creation Date
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Action
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($profiles as $profile)
+                                <tr>
+                                    <td class="ps-4">
+                                        <p class="text-xs font-weight-bold mb-0">1</p>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3">
+                                        </div>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">Admin</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">admin@softui.com</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">Admin</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <span class="text-secondary text-xs font-weight-bold">16/06/18</span>
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="#" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Edit user">
+                                            <i class="fas fa-user-edit text-secondary" aria-hidden="true"></i>
+                                        </a>
+                                        <span>
+<i class="cursor-pointer fas fa-trash text-secondary" aria-hidden="true"></i>
+</span>
+                                    </td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-            @endforeach
-
-                <div class="col-4 my-3">
-                    <a href="#">
-                        <div class="card">
-                            <div class="card-body p-4" style="align-self: center;">
-                                <i class="fal fa-plus-circle" style="font-size: 4rem"></i>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
+            </div>
         </div>
+
     </div>
 
 @endsection
--- resources/views/user/resume/index.blade.php
+++ resources/views/user/resume/index.blade.php
@@ -1,5 +1,58 @@
 @extends('layouts.user_type.auth')
 
 @section('content')
-<h1>Resumes</h1>
+
+    @php
+        $profiles = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        ];
+    @endphp
+
+    <div class="container-fluid">
+        <div class="row">
+
+            @foreach($profiles as $profile)
+                <div class="col-12 col-md-6 col-lg-4 my-3">
+                    <div class="card">
+                        <div class="card-header pb-0 px-3">
+                            <h6 class="mb-0">{{ __('Umarov Kamoliddin') }}</h6>
+                        </div>
+                        <div class="card-body pt-4 p-3">
+                            <ul style="list-style-type: none; padding: 0;">
+                                <li><i class="fal fa-user-edit"></i> Name: Kamoliddin</li>
+                                <li><i class="fal fa-user-edit"></i> Surname: Umarov</li>
+                                <li><i class="fal fa-calendar"></i> Birthday: {{ now()->format('d.m.Y') }}</li>
+                                <li><i class="fal fa-pen"></i> Updated at: {{ now()->format('d.m.Y') }}</li>
+                            </ul>
+
+                            <div class="row">
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <button type="button" class="btn btn-secondary"><i class="fas fa-trash"></i></button>
+                                    <button type="button" class="btn btn-secondary"><i class="fas fa-pen"></i></button>
+                                    <button type="button" class="btn btn-primary"><i class="fas fa-clipboard-check"></i> Make</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+            <div class="col-4 my-3">
+                <a href="#">
+                    <div class="card">
+                        <div class="card-body p-4" style="align-self: center;">
+                            <i class="fal fa-plus-circle" style="font-size: 4rem"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
 @endsection
